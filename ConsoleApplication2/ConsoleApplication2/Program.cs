e970c4c24f47b1f1f5f2290052694bc7b24dbe3b
DrunkFratBoy <diaryofannegank@gmail.com>
2015-05-26 09:46:25 -0400

    Added the ListDirectoryItems function

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -44,12 +44,39 @@ namespace ConsoleApplication2
             request.Method = WebRequestMethods.Ftp.UploadFile;
         }
 
+        private void ListDirectoryItems()
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://something.com");
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            request.Credentials = new NetworkCredential("test", "test");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            Console.WriteLine(reader.ReadToEnd());
+
+            Console.WriteLine("Directory List Complete, status {0}", response.StatusDescription);
+
+            reader.Close();
+            response.Close();
+        }
+
         static void Main(string[] args)
         {
+            // List contents of FTP Directory
+
+
             Program program = new Program();
             Console.WriteLine("Downloading..... Please wait....");
+            // List contents of FTP Directory
+            program.ListDirectoryItems();
+            // if there is items in the directory download those items
             program.Download("ftp://www.contoso.com/test.htm", "test.htm");
             Console.WriteLine("File Downloaded");
+
+
         }
     }
 }
