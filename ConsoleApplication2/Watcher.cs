45e6c85abd71ef4ca1af0170e1111d3722b47563
DrunkFratBoy <diaryofannegank@gmail.com>
2015-05-26 10:04:50 -0400

    Began writing File Watcher Class
    This class is going to watch files in a directory and once something has
    changed, it will know.

---
+++ ConsoleApplication2/Watcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Security.Permissions;
+
+public class Watcher
+{
+	[PermissionSet(SecurityAction.Demand, Name ="FullTrust")]
+    public static void Run()
+    {
+        string[] args = System.Environment.GetCommandLineArgs();
+
+        // if the directory is not specified, exit the program
+        if (args.Length != 2)
+        {
+
+            Console.WriteLine("Usage: Watcher.exe (directory)");
+            return;
+        }
+
+        // Create the file watcher and set its properties
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        watcher.Path = args[1];
+
+        // Watch for changes
+
+}
